ee200eb646b6c50aa60be71c87dd89080d05b6e8
pierrebenadon-ctrl <pierre.benadon@gmail.com>
2026-02-05 10:43:47 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 
+// On récupère la div "root" de ton index.html
 const rootElement = document.getElementById('root');
-if (!rootElement) throw new Error('Failed to find the root element');
+
+// Sécurité pour vérifier que l'élément existe bien
+if (!rootElement) {
+  throw new Error("L'élément root n'a pas été trouvé. Vérifie ton fichier index.html");
+}
 
 ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
